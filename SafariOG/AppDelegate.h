e0f4b1d5ec54c047ca3c94f7ca50f64b7aa78fff
Mingu Chu <mingu@mingus-macbook-pro.local>
2016-03-15 14:12:57 -0500

    Initial Commit

---
+++ SafariOG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariOG
+//
+//  Created by Mingu Chu on 3/15/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SafariOG/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SafariOG
+//
+//  Created by Mingu Chu on 3/15/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
